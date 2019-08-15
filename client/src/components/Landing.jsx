b6eb449cf295cb384524bc469ae99b362e34faa7
mattetress <mattetress@me.com>
2019-08-15 15:49:08 -0500

    add landing component

---
+++ client/src/components/Landing.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Landing() {
+  return (
+    <div className="home vc">
+      <div className="home-logo">
+        <h1 className="logo">ArtHub</h1>
+
+        <p>Please Sign Up or Log In</p>
+
+        <p>Log In with Facebook</p>
+      </div>
+    </div>
+  )
+}
