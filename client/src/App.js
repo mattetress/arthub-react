8e6550fc34ea94076d36a42f0eb1fec117f47bed
mattetress <mattetress@me.com>
2019-08-23 21:21:14 -0500

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import Landing from './components/Landing';
+import RegistrationContainer from './containers/RegistrationContainer';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Landing />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/signup" component={RegistrationContainer} />
+      </Switch>
+    </Router>
   );
 }
 
